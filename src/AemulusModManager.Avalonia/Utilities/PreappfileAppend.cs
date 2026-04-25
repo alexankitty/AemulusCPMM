e6ff680565519fe2ff37f3bdf8aaeb642b6f6da5
Alexandra <alexankitty@gmail.com>
2026-04-25 01:49:52 -0600

    fix solution structure formatting fix unreachable code in registration.

--- src/AemulusModManager.Avalonia/Utilities/PreappfileAppend.cs
+++ src/AemulusModManager.Avalonia/Utilities/PreappfileAppend.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Security.Cryptography;
-using System.Reflection;
-using AemulusModManager.Utilities;
-using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using AemulusModManager.Utilities;
 
 namespace AemulusModManager
 {
@@ -29,12 +29,14 @@ namespace AemulusModManager
 
             return checksumString;
         }
+
         public static void Validate(string path, string cpkLang)
         {
             var validated = true;
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = true;
-            startInfo.FileName = $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Dependencies\Preappfile\preappfile.exe";
+            startInfo.FileName =
+                $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Dependencies\Preappfile\preappfile.exe";
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
@@ -47,18 +49,27 @@ namespace AemulusModManager
                     process.Start();
                     process.WaitForExit();
                 }
-                foreach (var file in Directory.GetFiles($@"{path}\mods\preappfile\{Path.GetFileNameWithoutExtension(cpkLang)}", "*", SearchOption.AllDirectories))
+                foreach (
+                    var file in Directory.GetFiles(
+                        $@"{path}\mods\preappfile\{Path.GetFileNameWithoutExtension(cpkLang)}",
+                        "*",
+                        SearchOption.AllDirectories
+                    )
+                )
                 {
                     var folders = new List<string>(file.Split(char.Parse("\\")));
                     int idx = folders.IndexOf(Path.GetFileNameWithoutExtension(cpkLang));
-                    if (File.Exists($@"{path}\data00007\{string.Join("\\", folders.Skip(idx + 1).ToArray())}"))
+                    if (
+                        File.Exists(
+                            $@"{path}\data00007\{string.Join("\\", folders.Skip(idx + 1).ToArray())}"
+                        )
+                    )
                         Utilities.ParallelLogger.Log($"[INFO] Validated that {file} was appended");
                     else
                     {
                         Utilities.ParallelLogger.Log($"[WARNING] {file} not appended");
                         validated = false;
                     }
-
                 }
                 if (Directory.Exists($@"{path}\data00007"))
                     Directory.Delete($@"{path}\data00007", true);
@@ -72,65 +83,112 @@ namespace AemulusModManager
                     process.Start();
                     process.WaitForExit();
                 }
-                foreach (var file in Directory.GetFiles($@"{path}\mods\preappfile\movie", "*", SearchOption.AllDirectories))
+                foreach (
+                    var file in Directory.GetFiles(
+                        $@"{path}\mods\preappfile\movie",
+                        "*",
+                        SearchOption.AllDirectories
+                    )
+                )
                 {
                     var folders = new List<string>(file.Split(char.Parse("\\")));
                     int idx = folders.IndexOf("movie");
-                    if (File.Exists($@"{path}\movie00003\{string.Join("\\", folders.Skip(idx + 1).ToArray())}"))
+                    if (
+                        File.Exists(
+                            $@"{path}\movie00003\{string.Join("\\", folders.Skip(idx + 1).ToArray())}"
+                        )
+                    )
                         Utilities.ParallelLogger.Log($@"[INFO] Validated appended {file}");
                     else
                     {
                         Utilities.ParallelLogger.Log($@"[WARNING] {file} not appended");
                         validated = false;
                     }
-
                 }
                 if (Directory.Exists($@"{path}\movie00003"))
                     Directory.Delete($@"{path}\movie00003", true);
             }
             if (!validated)
             {
-                Utilities.ParallelLogger.Log($"[WARNING] Not all appended files were validated, trying again");
+                Utilities.ParallelLogger.Log(
+                    $"[WARNING] Not all appended files were validated, trying again"
+                );
                 Append(path, cpkLang);
                 Validate(path, cpkLang);
             }
         }
+
         public static void Append(string path, string cpkLang)
         {
             // Check if required files are there
-            if (!File.Exists($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Dependencies\preappfile\preappfile.exe"))
+            if (
+                !File.Exists(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Dependencies\preappfile\preappfile.exe"
+                )
+            )
             {
-                Utilities.ParallelLogger.Log($@"[ERROR] Couldn't find Dependencies\preappfile\preappfile.exe. Please check if it was blocked by your anti-virus.");
+                Utilities.ParallelLogger.Log(
+                    $@"[ERROR] Couldn't find Dependencies\preappfile\preappfile.exe. Please check if it was blocked by your anti-virus."
+                );
                 return;
             }
 
             if (!File.Exists($@"{path}\{cpkLang}"))
             {
-                Utilities.ParallelLogger.Log($@"[ERROR] Couldn't find {path}\{cpkLang} for appending.");
+                Utilities.ParallelLogger.Log(
+                    $@"[ERROR] Couldn't find {path}\{cpkLang} for appending."
+                );
                 return;
             }
             // Backup cpk if not backed up already
-            if (!File.Exists($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\{cpkLang}"))
+            if (
+                !File.Exists(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\{cpkLang}"
+                )
+            )
             {
                 Utilities.ParallelLogger.Log($@"[INFO] Backing up {cpkLang}.cpk");
                 File.Copy($@"{path}\{cpkLang}", $@"Original\Persona 4 Golden\{cpkLang}");
             }
             // Copy original cpk back if different
-            if (GetChecksumString($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\{cpkLang}") != GetChecksumString($@"{path}\{cpkLang}"))
+            if (
+                GetChecksumString(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\{cpkLang}"
+                ) != GetChecksumString($@"{path}\{cpkLang}")
+            )
             {
                 Utilities.ParallelLogger.Log($@"[INFO] Reverting {cpkLang} back to original");
-                File.Copy($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\{cpkLang}", $@"{path}\{cpkLang}", true);
+                File.Copy(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\{cpkLang}",
+                    $@"{path}\{cpkLang}",
+                    true
+                );
             }
-            if (!File.Exists($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk"))
+            if (
+                !File.Exists(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk"
+                )
+            )
             {
                 Utilities.ParallelLogger.Log($@"[INFO] Backing up movie.cpk");
-                File.Copy($@"{path}\movie.cpk", $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk");
+                File.Copy(
+                    $@"{path}\movie.cpk",
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk"
+                );
             }
             // Copy original cpk back if different
-            if (GetChecksumString($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk") != GetChecksumString($@"{path}\movie.cpk"))
+            if (
+                GetChecksumString(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk"
+                ) != GetChecksumString($@"{path}\movie.cpk")
+            )
             {
                 Utilities.ParallelLogger.Log($@"[INFO] Reverting movie.cpk back to original");
-                File.Copy($@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk", $@"{path}\movie.cpk", true);
+                File.Copy(
+                    $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Original\Persona 4 Golden\movie.cpk",
+                    $@"{path}\movie.cpk",
+                    true
+                );
             }
             // Delete modified pacs
             if (File.Exists($@"{path}\data00007.pac"))
@@ -146,13 +204,19 @@ namespace AemulusModManager
 
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = true;
-            startInfo.FileName = $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Dependencies\preappfile\preappfile${(OperatingSystem.IsWindows() ? ".exe" : "")}";
+            startInfo.FileName =
+                $@"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\Dependencies\preappfile\preappfile${(OperatingSystem.IsWindows() ? ".exe" : "")}";
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
             startInfo.UseShellExecute = false;
-            if (Directory.Exists($@"{path}\mods\preappfile\{Path.GetFileNameWithoutExtension(cpkLang)}"))
+            if (
+                Directory.Exists(
+                    $@"{path}\mods\preappfile\{Path.GetFileNameWithoutExtension(cpkLang)}"
+                )
+            )
             {
                 Utilities.ParallelLogger.Log($@"[INFO] Appending to {cpkLang}");
-                startInfo.Arguments = $@"-i  ""{path}\mods\preappfile\{Path.GetFileNameWithoutExtension(cpkLang)}"" -a ""{path}\{cpkLang}"" -o ""{path}\{cpkLang}"" --pac-index 7";
+                startInfo.Arguments =
+                    $@"-i  ""{path}\mods\preappfile\{Path.GetFileNameWithoutExtension(cpkLang)}"" -a ""{path}\{cpkLang}"" -o ""{path}\{cpkLang}"" --pac-index 7";
                 using (Process process = new Process())
                 {
                     process.StartInfo = startInfo;
@@ -163,7 +227,8 @@ namespace AemulusModManager
             if (Directory.Exists($@"{path}\mods\preappfile\movie"))
             {
                 Utilities.ParallelLogger.Log($@"[INFO] Appending to movie");
-                startInfo.Arguments = $@"-i  ""{path}\mods\preappfile\movie"" -a ""{path}\movie.cpk"" -o ""{path}\movie.cpk"" --pac-index 3";
+                startInfo.Arguments =
+                    $@"-i  ""{path}\mods\preappfile\movie"" -a ""{path}\movie.cpk"" -o ""{path}\movie.cpk"" --pac-index 3";
                 using (Process process = new Process())
                 {
                     process.StartInfo = startInfo;
--- src/AemulusModManager.Avalonia/Utilities/ProtocolRegistration.cs
+++ src/AemulusModManager.Avalonia/Utilities/ProtocolRegistration.cs
@@ -1,106 +1,79 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Collections.Concurrent;
 
 namespace AemulusModManager.Avalonia.Utilities;
 
 public static class ProtocolRegistration
 {
     private static readonly string ProtocolName = "aemulus";
-    private static readonly bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-    private static readonly bool isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-    private static readonly bool isOSX = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
-    private static readonly string platform = isWindows ? "win" :
-                                      isLinux ? "linux" :
-                                      isOSX ? "osx" : "unknown";
+
     public static void RegisterProtocol()
     {
-        switch (platform)
-        {
-            case string s when s.StartsWith("win"):
-                RegisterProtocolWindows();
-                break;
-            case string s when s.StartsWith("linux"):
-                RegisterProtocolLinux();
-                break;
-            default:
-                Console.WriteLine($"Protocol registration not supported on platform: {platform}");
-                break;
-        }
-    }
-    public static void RegisterProtocolWindows()
-    {
-        if(!isWindows)
-        {
-            return;
-        }
+#if WINDOWS
         try
         {
-            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey($"SOFTWARE\\Classes\\{ProtocolName}");
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(
+                $"SOFTWARE\\Classes\\{ProtocolNName}"
+            );
             key.SetValue("", "URL:Aemulus Protocol");
             key.SetValue("URL Protocol", "");
-            key.CreateSubKey(@"shell\open\command").SetValue("", $"\"{Process.GetCurrentProcess().MainModule.FileName}\" \"%1\"");
+            key.CreateSubKey(@"shell\open\command")
+                .SetValue("", $"\"{Process.GetCurrentProcess().MainModule.FileName}\" \"%1\"");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Failed to register protocol: {ex}");
         }
-    }
-    public static void RegisterProtocolLinux()
-    {
-        if(!isLinux)
-        {
-            return;
-        }
-        // Linux protocol registration typically involves creating a .desktop file and updating the MIME database.
-        // This is a simplified example and may require additional steps for a complete implementation.
-        string desktopFileContent = $"[Desktop Entry]\n"+
-                                    $"Name=Aemulus Protocol Handler\n" +
-                                    $"Exec={Process.GetCurrentProcess().MainModule.FileName} %u\n" +
-                                    $"Type=Application\n" +
-                                    $"MimeType=x-scheme-handler/{ProtocolName}\n";
+#else
+        string desktopFileContent =
+            $"[Desktop Entry]\n"
+            + $"Name=Aemulus Protocol Handler\n"
+            + $"Exec={Process.GetCurrentProcess().MainModule.FileName} %u\n"
+            + $"Type=Application\n"
+            + $"MimeType=x-scheme-handler/{ProtocolName}\n";
 
-        string desktopFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "applications", $"{ProtocolName}-handler.desktop");
+        string desktopFilePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".local",
+            "share",
+            "applications",
+            $"{ProtocolName}-handler.desktop"
+        );
         try
         {
             File.WriteAllText(desktopFilePath, desktopFileContent);
             // Update the MIME database (this may require additional permissions)
-            Process.Start("update-desktop-database", $"{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "applications")}");
-            Process.Start("xdg-mime", $"default {ProtocolName}-handler.desktop x-scheme-handler/{ProtocolName}");
+            Process.Start(
+                "update-desktop-database",
+                $"{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "applications")}"
+            );
+            Process.Start(
+                "xdg-mime",
+                $"default {ProtocolName}-handler.desktop x-scheme-handler/{ProtocolName}"
+            );
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Failed to register protocol: {ex}");
         }
-    }
-    public static bool CheckRegistration()
-    {
-        switch (platform)
-        {
-            case string s when s.StartsWith("win"):
-                return CheckRegistrationWindows();
-            case string s when s.StartsWith("linux"):
-                return CheckRegistrationLinux();
-            default:
-                Console.WriteLine($"Protocol registration check not supported on platform: {platform}");
-                return false;
-        }
+#endif
     }
 
-    public static bool CheckRegistrationWindows()
+    public static bool CheckRegistration()
     {
-        if(!isWindows)
-        {
-            return false;
-        }
+#if WINDOWS
+
         try
         {
-            Microsoft.Win32.RegistryKey? key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey($"SOFTWARE\\Classes\\{ProtocolName}");
-            if(key == null || key.GetValue("")?.ToString() != "URL:Aemulus Protocol")
+            Microsoft.Win32.RegistryKey? key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(
+                $"SOFTWARE\\Classes\\{ProtocolName}"
+            );
+            if (key == null || key.GetValue("")?.ToString() != "URL:Aemulus Protocol")
             {
                 return false;
             }
@@ -110,21 +83,26 @@ public static class ProtocolRegistration
             Console.WriteLine($"Failed to check protocol registration: {ex}");
             return false;
         }
-        return true;
-    }
 
-    public static bool CheckRegistrationLinux()
-    {
-        if(!isLinux)
-        {
-            return false;
-        }
-        string desktopFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share", "applications", $"{ProtocolName}-handler.desktop");
+        return true;
+
+#else
+
+        string desktopFilePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".local",
+            "share",
+            "applications",
+            $"{ProtocolName}-handler.desktop"
+        );
+
         if (!File.Exists(desktopFilePath))
         {
             return false;
         }
+
         string desktopFileContent = File.ReadAllText(desktopFilePath);
         return desktopFileContent.Contains($"MimeType=x-scheme-handler/{ProtocolName}");
+#endif
     }
 }
